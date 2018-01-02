b65df52f60c8be697e70c661fdb378897cbcf701
Jesus Sanchez <scysco@gmail.com>
2018-01-02 12:51:41 -0600

    fixes

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ sudo ln -s $HOME/.dotfiles/pacman.conf /etc/pacman.conf
 sudo pacman -Syu
 sudo pacman -S yaourt
 #install enviroment
-sudo pacman -S xorg xorg-xinit xorg-server xmonad xmonad-contrib xmobar dzen2 dmenu mesa mesa-demos libxss trayer xdg-user-dirs pulseaudio pulseaudio-alsa pacvucontrol pamixer feh ranger libcaca highlight atool w3m lynx poppler transmission-cli mediainfo perl-image-exiftool firefox numlockx
+sudo pacman -S xorg alsa-utils xorg-xinit xmonad xmonad-contrib xmobar dzen2 dmenu mesa mesa-demos libxss trayer xdg-user-dirs pulseaudio pulseaudio-alsa pavucontrol pamixer feh ranger libcaca highlight atool w3m lynx poppler transmission-cli mediainfo perl-image-exiftool firefox numlockx
 ranger --copy-config=all
 rm $HOME/.config/ranger/rc.conf
 ln -s $HOME/.dotfiles/rc.conf $HOME/.config/ranger/rc.conf
 xdg-user-dirs-update
 yaourt -S notify-osd-customizable notifyconf termite-ranger-fix-git systemd-numlockontty
 systemctl enable numLockOnTty
-mkdir -p /etc/systemd/system/getty@tty1.service.d/
-sudo ln -s ~/.dotfiles/autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+sudo cp ~/.dotfiles/autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf
 sudo pacman -S ttf-liberation ttf-bitstream-vera ttf-dejavu ttf-droid ttf-freefont artwiz-fonts
 ln -s $HOME/.dotfiles/notify-osd $HOME/.notify-osd
 mkdir -p $HOME/.config/compton
@@ -26,7 +26,7 @@ sudo pacman -S zsh
 chsh -s /bin/zsh
 curl -L http://install.ohmyz.sh | sh
 ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/lambda-mod.zsh-theme
+ln -s $HOME/.dotfiles/lambda-mod.zsh-theme $HOME/.oh-my-zsh/themes/lambda-mod.zsh-theme
 ln -s $HOME/.dotfiles/stalonetrayrc $HOME/.stalonetrayrc
 #apps config
 # termite vim, npm, ...
